3c1358eb35171fa4e0befe3b275b9912bdd4665c
Ionut Trandafir <ionut.trandafir04@gmail.com>
2021-08-28 16:29:36 +0300

    added like functionality

--- website/models.py
+++ website/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     date_created =  db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
     
     
 class Post(db.Model):
@@ -21,6 +22,7 @@ class Post(db.Model):
     date_created =  db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)  # ondelete="CASCADE" deletes the post if user is deleted
     comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)  
@@ -29,4 +31,8 @@ class Comment(db.Model):
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)  # ondelete="CASCADE" deletes the post if user is deleted
     post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)  
 
-
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    date_created =  db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)  # ondelete="CASCADE" deletes the post if user is deleted
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)  
