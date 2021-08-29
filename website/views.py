5f7b4963ca0b2dfcd2c4e9263fcbf1c1fc9aa160
Jonny Rose <69922806+jonny-rose@users.noreply.github.com>
2021-08-29 15:53:53 +0300

    Revert "Javascript"

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Post, User, Comment, Like
 from . import db
@@ -91,14 +91,14 @@ def delete_comment(comment_id):
         
     return redirect(url_for('views.home'))
 
-@views.route('/like-post/<post_id>', methods=['POST'])
+@views.route('/like-post/<post_id>', methods=['GET'])
 @login_required
 def like(post_id):
-    post = Post.query.filter_by(id=post_id).first()
+    post = Post.query.filter_by(id=post_id)
     like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
     
     if not post:
-        return jsonify({'error': 'Post does not exist.'}, 400)
+        flash('Post does not exist.', category='error')
     elif like:
         db.session.delete(like)
         db.session.commit()
@@ -107,5 +107,5 @@ def like(post_id):
         db.session.add(like)
         db.session.commit()
         
-    return jsonify({'likes': len(post.likes), 'liked': current_user.id in map(lambda x: x.author, post.likes)})
+    return redirect(url_for('views.home'))
 
