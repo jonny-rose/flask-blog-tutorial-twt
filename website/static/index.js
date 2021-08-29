2953e8d9f81681d04cb78b72b5b4b90e3dc27a55
Ionut Trandafir <ionut.trandafir04@gmail.com>
2021-08-29 15:46:37 +0300

    added js functionality for like post button

--- website/static/index.js
+++ website/static/index.js
@@ -1,3 +1,13 @@
-function like(post_id) {
-    console.log(post_id)
+function like(postId) {
+    const likeCount = document.getElementById(`likes-count-${postId}`);
+    const likeButton = document.getElementById(`like-button-${postId}`);
+
+    fetch(`/like-post/${postId}`, {method: 'POST'}).then((res) => res.json()).then((data) => {
+        likeCount.innerHTML = data["likes"];
+        if (data["liked"] === true) {
+            likeButton.className = "fas fa-thumbs-up";
+        } else {
+            likeButton.className = "far fa-thumbs-up";
+        }
+    }).catch((e) => alert("could not like post."));
 }
